daa293f181d5ef0c9bbe842fd30757f371e20991
vkrishnanb <vkrishnanb@yahoo.com>
2024-10-25 18:40:32 +0530

    Logo added to index

--- javabasiclessons/lessons/scripts/index.js
+++ javabasiclessons/lessons/scripts/index.js
@@ -10,11 +10,18 @@ function loadPage(url, loc) {
 document.addEventListener("DOMContentLoaded", function() {
     // Code to execute when the DOM is fully loaded
     const timestamp = new Date().getTime(); // Generate a unique query string;
-    const url='javabasiclessons/lessons/tree.html';
-    const loc='tree';
-    fetch(url+'?v=' + timestamp)
+    const tree_url='javabasiclessons/lessons/tree.html';
+    const tree_loc='tree';
+    const home_url='javabasiclessons/lessons/home.html';
+    const home_loc='content';
+    fetch(tree_url+'?v=' + timestamp)
         .then(response => response.text())
         .then(data => {
-            document.getElementById(loc).innerHTML = data;
+            document.getElementById(tree_loc).innerHTML = data;
+        });
+    fetch(home_url+'?v=' + timestamp)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(home_loc).innerHTML = data;
         });
 });
